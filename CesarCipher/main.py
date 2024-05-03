9c851dcfa74f6d387100c8244759821ba3d9867b
Rad Antonio <tonirad@icloud.com>
2024-05-03 15:11:44 +0300

    caesar cipher end

---
+++ CesarCipher/main.py
@@ -0,0 +1,34 @@
+from art import logo
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+print(logo)
+def ceaser(start_text, shift_amount, cipher_direction):
+    end_text = ""
+    if cipher_direction == 'decode':
+        shift_amount *= -1
+    for char in start_text:
+        if char in alphabet:
+            position = alphabet.index(char)
+            new_position = position + shift_amount
+            end_text += alphabet[new_position]
+        else:
+            end_text += char
+    print(f"The {cipher_direction}d text is {end_text}")
+
+should_continue = True
+
+while should_continue:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    shift = shift % 26
+    ceaser(text, shift, direction)
+    result = input("Do you want to try again?(y/n)\n").lower()
+    if result == 'n':
+        should_continue = False
+
+
+
+
+
+
